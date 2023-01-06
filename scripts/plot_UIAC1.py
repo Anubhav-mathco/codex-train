3bdf809e28b00b46f859699cc4dfe442d1ca68ee
Anubhav-mathco <122104045+Anubhav-mathco@users.noreply.github.com>
2023-01-06 20:26:19 +0530

    Add files via upload

---
+++ scripts/plot_UIAC1.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#BEGIN CUSTOM CODE BELOW...
+
+#put your output in this response param for connecting to downstream widgets
+response_0 = """
+
+import plotly.express as px
+import pandas as pd
+import json
+import plotly.io as io
+import sklearn
+from sklearn import datasets
+
+def getGraph():
+    
+    data = datasets.load_wine()
+    df = pd.DataFrame(data.data, columns=data.feature_names)
+    fig = px.scatter(df, x="alcohol", y="malic_acid", title = "Bubble Plot")
+
+    # fig.show()
+    return io.to_json(fig)
+
+dynamic_outputs = getGraph()
+"""
+
+
+#END CUSTOM CODE
+
